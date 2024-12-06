9927e2229b42fcc355170946687a7c3d6b07ec6f
Tracy Tuhakaraina <tracytmail@gmail.com>
2024-12-06 21:25:50 +1100

    Completed random generator with click event

--- js/script.js
+++ js/script.js
@@ -3,11 +3,38 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function (params) {
+const getImage = async function () {
     const res = await fetch(
         "https://picsum.photos/v2/list?limit=100"
+
     );
     const images = await res.json();
     console.log(images);
-};
-getImage();
+
+    selectRandomImage(images);
+}
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length)
+    const randomImage = images[randomIndex];
+    //console.log(randomIndex);
+    //console.log(randomImage);
+    displayImage(randomImage);
+}
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function () {
+    getImage();
+
+});
+
+
+
+
